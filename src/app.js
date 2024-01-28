9fc1450ba9976b46c206790bbd9fbc2a8dbe7f1b
Hamodi-9 <113707892+Hamodi-9@users.noreply.github.com>
2024-01-28 10:41:24 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const authenticate = require('./auth');
+
+const passport = require('passport');
 
 // author and version from our package.json file
 const { author, version } = require('../package.json');
@@ -9,7 +12,7 @@ const { author, version } = require('../package.json');
 const logger = require('./logger');
 const pino = require('pino-http')({
   logger,
-});
+});  
 
 // Create an express app instance
 const app = express();
@@ -17,25 +20,27 @@ const app = express();
 // Use pino logging middleware
 app.use(pino);
 
+
+
+
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
+
+// Define our routes
+app.use('/', require('./routes'));
 // Use helmetjs security middleware
 app.use(helmet());
 
 // Use CORS middleware
 app.use(cors());
 
-// Use gzip/deflate compression middleware
-app.use(compression());
 
-// Define a health check route
-app.get('/', (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache');
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/Hamodi-9/fragments',
-    version,
-  });
-});
+
 
 // 404 middleware
 app.use((req, res) => {
@@ -47,8 +52,12 @@ app.use((req, res) => {
     },
   });
 });
-// eslint-disable-line no-unused-vars
+
+
+
+
 // Error-handling middleware
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || 'unable to process request';
