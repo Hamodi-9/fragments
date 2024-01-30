813d77d87e4124c7b17f965996cbf7a6b097eaa1
Hamodi-9 <113707892+Hamodi-9@users.noreply.github.com>
2024-01-30 09:47:23 +0530

    Add files via upload

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,22 @@
 // src/logger.js
 
-// Use `info` as our standard log level if not specified
-const options = { level: process.env.LOG_LEVEL || 'info' };
+let options = {};
 
-// If we're doing `debug` logging, make the logs easier to read
-if (options.level === 'debug') {
-  // https://github.com/pinojs/pino-pretty
-  options.transport = {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  };
+// Check if process object is available (running in Node.js environment)
+if (typeof process !== 'undefined' && process.env) {
+  // Use `info` as our standard log level if not specified
+  options = { level: process.env.LOG_LEVEL || 'info' };
+
+  // If we're doing `debug` logging, make the logs easier to read
+  if (options.level === 'debug') {
+    // https://github.com/pinojs/pino-pretty
+    options.transport = {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    };
+  }
 }
 
 // Create and export a Pino Logger instance:
